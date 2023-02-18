8d29a1100df62abd406a78daaff9e569928e1609
Jonas <publik.froeller.jonas@gmx.at>
2023-02-18 10:27:25 +0100

    portfolio 0.0.1

---
+++ portfolio/svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: '../portfolio-build',
+			assets: '../portfolio-build/assets',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+
+export default config;
