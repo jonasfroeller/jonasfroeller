d78f888a398d0e76e5a7e4b21f898b1b66d18f57
Jonas <publik.froeller.jonas@gmx.at>
2023-02-18 11:33:44 +0100

    actions config change

--- portfolio/svelte.config.js
+++ portfolio/svelte.config.js
@@ -4,8 +4,8 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: '../portfolio-build',
-			assets: '../portfolio-build/assets',
+			pages: 'build',
+			assets: 'build/assets',
 			fallback: null,
 			precompress: false,
 			strict: true
