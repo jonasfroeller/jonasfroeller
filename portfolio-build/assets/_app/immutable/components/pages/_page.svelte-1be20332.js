a0f659a2f579f2970c8f1ca3d537d6f4360c5f9c
Jonas <publik.froeller.jonas@gmx.at>
2023-02-18 10:59:27 +0100

    portfolio build-test 0.0.4

---
+++ portfolio-build/assets/_app/immutable/components/pages/_page.svelte-1be20332.js
@@ -0,0 +1,1 @@
+import{S as l,i,s as c,k as h,q as m,l as d,m as p,r as _,h as r,b as f,H as u,C as n}from"../../chunks/index-65bc829e.js";function x(o){let e,s;return{c(){e=h("h1"),s=m("Test")},l(t){e=d(t,"H1",{});var a=p(e);s=_(a,"Test"),a.forEach(r)},m(t,a){f(t,e,a),u(e,s)},p:n,i:n,o:n,d(t){t&&r(e)}}}class q extends l{constructor(e){super(),i(this,e,null,x,c,{})}}export{q as default};
