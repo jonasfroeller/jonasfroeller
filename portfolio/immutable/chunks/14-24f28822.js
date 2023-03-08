95b885588d08c5aaf5bf95ffe7a709cd322ab46e
jonasfroeller <121523551+jonasfroeller@users.noreply.github.com>
2023-03-08 20:00:01 +0000

    Deploying to portfolio from @ jonasfroeller/jonasfroeller@9bd715afc0f5026eab6bea3fd15e224fbff4d061 🚀

--- portfolio/immutable/chunks/14-24f28822.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_lang_/projects/_page.svelte-74d4a4fc.js";export{t as component};
---
+++ portfolio/immutable/chunks/2-4ffef642.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-eaa62d4f.js";import{default as t}from"../components/pages/_lang_/_layout.svelte-948415ce.js";export{t as component,r as universal};
