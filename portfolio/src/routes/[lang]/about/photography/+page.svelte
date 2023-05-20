15f5162a6f93272eb0b5fb4aa857721d4580a2b4
Jonas <publik.froeller.jonas@gmx.at>
2023-05-20 20:36:01 +0200

    image loading animation

--- portfolio/src/routes/[lang]/about/photography/+page.svelte
+++ portfolio/src/routes/[lang]/about/photography/+page.svelte
@@ -20,7 +20,7 @@
 	import photo11 from '$image/photography/photography-11.jpg';
 	import photo12 from '$image/photography/photography-12.jpg';
 
-	const photos = [
+	let photos = [
 		photo1,
 		photo2,
 		photo3,
@@ -34,6 +34,20 @@
 		photo11,
 		photo12
 	];
+
+	let imagesLoaded = false;
+
+	photos.forEach((photo, i) => {
+		if (browser) {
+			const img = new Image();
+			img.onload = () => {
+				if (i === photos.length - 1) {
+					imagesLoaded = true;
+				}
+			};
+			img.src = photo;
+		}
+	});
 
 	$: currentPhoto = '#photography-1';
 
@@ -56,21 +70,33 @@
 </section>
 
 <section class="flex justify-center">
-	<div class="w-[60dvw] w-[60vw]">
+	<div class="w-[60dvw]">
 		<div class="carousel rounded-2xl mt-4">
 			{#each photos as photo, i}
 				<div id="photography-{i + 1}" class="carousel-item w-full rounded-2xl pt-4">
-					<img src={photo} alt="photography-{i + 1}" class="w-full object-cover rounded-2xl" />
+					{#if imagesLoaded}
+						<img src={photo} alt="photography-{i + 1}" class="w-full object-cover rounded-2xl" />
+					{:else}
+						<div class="flex flex-col justify-center w-full">
+							<div class="loading-wrapper">
+								<div class="loading-animation">
+									<div class="border-4" />
+								</div>
+							</div>
+							{#if browser && navigator && navigator.userAgent && !navigator.userAgent.includes('js_disabled')}
+								<div class="flex justify-center"><p><em>Couldn't load images!</em></p></div>
+							{:else}
+								<noscript
+									>Sorry, your browser does not support JavaScript or you deactivated it! Features
+									of the website that do not work without JS: style-mode, language-change, settings
+									save...
+								</noscript>
+							{/if}
+						</div>
+					{/if}
 				</div>
 			{:else}
-				{#if browser && navigator && navigator.userAgent && !navigator.userAgent.includes('js_disabled')}
-					<p><em>Couldn't load images!</em></p>
-				{:else}
-					<noscript
-						>Sorry, your browser does not support JavaScript or you deactivated it! Features of the
-						website that do not work without JS: style-mode, language-change, settings save...
-					</noscript>
-				{/if}
+				<div class="flex justify-center"><p><em>Couldn't load images!</em></p></div>
 			{/each}
 		</div>
 		<div class="flex justify-center flex-wrap w-full py-2 gap-2">
@@ -80,7 +106,7 @@
 						href="#photography-{i + 1}"
 						on:click={() => (currentPhoto = `#photography-${i + 1}`)}
 						class="btn btn-s text-lg text-primary {currentPhoto == '#photography-' + (i + 1)
-							? 'border border-transparent night:border-white cmyk:border-black hover:border-primary'
+							? 'border border-primary hover:border-primary bg-transparent hover:bg-transparent'
 							: ''}">{i + 1}</a
 					>
 				{:else}
@@ -88,7 +114,7 @@
 						href="#photography-{i + 1}"
 						on:click={() => (currentPhoto = `#photography-${i + 1}`)}
 						class="btn btn-s text-lg text-secondary {currentPhoto == '#photography-' + (i + 1)
-							? 'border border-transparent night:border-white cmyk:border-black hover:border-secondary'
+							? 'border border-secondary hover:border-secondary bg-transparent hover:bg-transparent'
 							: ''}">{i + 1}</a
 					>
 				{/if}
@@ -102,3 +128,50 @@
 		<p><em>~ {$translation.Pages.about.photography.note()}</em></p>
 	</div>
 </section>
+
+<style>
+	@keyframes loading-animation {
+		0% {
+			transform: translate(-50%, -50%) rotate(0deg);
+		}
+
+		100% {
+			transform: translate(-50%, -50%) rotate(360deg);
+		}
+	}
+
+	.loading-animation div {
+		position: absolute;
+		width: 30px;
+		height: 30px;
+		border-top-color: transparent;
+		border-radius: 50%;
+	}
+
+	.loading-animation div {
+		animation: loading-animation 1s linear infinite;
+		top: 100px;
+		left: 100px;
+	}
+
+	.loading-wrapper {
+		width: 200px;
+		height: 200px;
+		position: relative;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+
+	.loading-animation {
+		width: 100%;
+		height: 100%;
+		position: relative;
+		transform: translateZ(0) scale(1);
+		backface-visibility: hidden;
+		transform-origin: 0 0;
+	}
+
+	.loading-animation div {
+		box-sizing: content-box;
+	}
+</style>
