b83f60b3b7c368c1cf4798be5c8619326a019174
jonasfroeller <121523551+jonasfroeller@users.noreply.github.com>
2023-11-10 21:41:11 +0000

    Deploying to portfolio from @ jonasfroeller/jonasfroeller@ec8164792b5d0f721b246307371dc07023da802c 🚀

--- portfolio/immutable/chunks/navigation.c08761c3.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.94c3b305.js";const e=o("goto");export{e as g};
---
+++ portfolio/immutable/chunks/navigation.c544d681.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.89a9d700.js";const a=o("goto"),i=o("invalidate_all");export{a as g,i};
